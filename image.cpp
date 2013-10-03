5e4d20fe34782d9f1a43cb8f6e88793a2c1a6f8f
Hachem Zerdia <hachem073@gmail.com>
2013-10-03 14:57:35 +0100

    create image class & add att and functions member

---
+++ image.cpp
@@ -0,0 +1,11 @@
+#include "image.h"
+
+Image::Image(QObject *parent) :
+  QObject(parent)
+{
+
+}
+
+Image(QString realPath,QObject *parent = 0) {
+
+}
---
+++ image.h
@@ -0,0 +1,32 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include <QObject>
+#include <QString>
+#include <QImage>
+
+class Image : public QObject
+{
+  Q_OBJECT
+public:
+  explicit Image (QObject *parent = 0);
+  Image(QString realPath,QObject *parent = 0);
+  void setRealPath(QString path);
+  QString getRealPath() const;
+  void setTempPath(QString path);
+  QString getTempPath() const;
+  QImage crop(); // crop the image and save it in temp path.
+
+signals:
+  void wb_finished(); // emited when black_white() finiched.
+  
+public slots:
+  void black_white();
+
+private:
+  QString realPath;
+  QString tempPath;
+  
+};
+
+#endif // IMAGE_H
