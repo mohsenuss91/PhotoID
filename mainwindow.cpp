55a7fd4d3b168b9485ed0f82d10a4a7c9a017e66
Hachem Zerdia <hachem073@gmail.com>
2013-10-03 13:49:07 +0100

    finish of the design

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // hide minimize and maximize buttons.
+    this->setWindowFlags(Qt::Tool | Qt::WindowTitleHint | Qt::WindowCloseButtonHint | Qt::CustomizeWindowHint);
+    // Quit App when close the window.
+    this->setAttribute(Qt::WA_QuitOnClose);
 }
 
 MainWindow::~MainWindow()
