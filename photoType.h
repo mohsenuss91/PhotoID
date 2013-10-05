b0cc9af9db36af3f7a6645e740467d7e6bfac2a6
Hachem Zerdia <hachem073@gmail.com>
2013-10-05 13:17:03 +0100

    Add record represent type of the photo

---
+++ photoType.h
@@ -0,0 +1,13 @@
+#ifndef PHOTOTYPE_H
+#define PHOTOTYPE_H
+
+#include <QString>
+
+struct PhotoType{
+    int NumType;
+    QString Text;
+    int Width;
+    int Height;
+};
+
+#endif // PHOTOTYPE_H
